22502260ea95aeed6ddf1f820a3c2fe52041de88
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2022-06-09 11:15:35 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        padding: '1rem',
+        center: 'true',
+      },
+    },
+  },
+  plugins: [],
+}
